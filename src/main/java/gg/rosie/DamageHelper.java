08b90285b865cde149d1435907cd08249547a38b
Rosie Hart <playinggameswithfriends@gmail.com>
2024-03-13 03:04:30 -0600

    get rid of debug stuff

--- src/main/java/gg/rosie/DamageHelper.java
+++ src/main/java/gg/rosie/DamageHelper.java
@@ -2,13 +2,11 @@ package gg.rosie;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.util.Identifier;
-
 public class DamageHelper implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    // public static final Logger LOGGER = LoggerFactory.getLogger("damagehelper");
+	// public static final Logger LOGGER = LoggerFactory.getLogger("damagehelper");
 
 	@Override
 	public void onInitialize() {
@@ -17,5 +15,9 @@ public class DamageHelper implements ModInitializer {
 		// Proceed with mild caution.
 
 		// LOGGER.info("Hello Fabric world!");
+		/*
+		 * CustomItemCrits.add("minecraft:air", (s, a) ->
+		 * System.out.println("air crit in lib"));
+		 */
 	}
 }
